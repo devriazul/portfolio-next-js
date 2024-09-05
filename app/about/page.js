8f0a7f7c1819aecb7b6108339fb654149608a141
devriazul <riazul56@gmail.com>
2024-09-05 12:16:24 +0600

    issues fixed

--- app/about/page.js
+++ app/about/page.js
@@ -7,7 +7,7 @@ export default function About() {
     <div className="h-screen w-screen bg-slate-800 pt-5">
       
       <p className="m-auto text-xl font-bold text-center">
-        Hello, I'm Md Riazul Islam, a web design and development professional.
+        Hello, I'm Riazul Islam, a web design and development professional.
         <br />
         With lots of experience, I have gained extensive expertise in handling client projects and possess strong leadership and communication skills.
         <br />
