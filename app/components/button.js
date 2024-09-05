8c6e870a7d8a6aa9dfbf2d90753c8fdd1f7dc228
devriazul <riazul56@gmail.com>
2024-09-05 17:18:28 +0600

    error fix

--- app/components/button.js
+++ app/components/button.js
@@ -2,7 +2,7 @@
 export default function Button() {
     return (
         <div className="text-center my-5 pt-5">
-            <button onClick={() => console.log("Button Clicked")} className="btn bg-green-800 rounded-lg text-xl font-bold">
+            <button onClick={() => console.log("Button Clicked")} className="btn bg-green-800 rounded-lg text-xl px-3 py-2 font-bold">
                 Click Here
             </button>
         </div>
