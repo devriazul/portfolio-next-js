704df0bef9fa531e64ab0283b025894f10f783d6
devriazul <riazul56@gmail.com>
2024-09-05 11:40:37 +0600

    not found page update

--- app/blogs/[id]/page.js
+++ app/blogs/[id]/page.js
@@ -1,10 +1,14 @@
+import { notFound } from "next/navigation";
 import React from "react";
 
 export default function BlogPage({ params }) {
     const id = params.id;
+    if (id > 3) {
+        notFound()
+    }
     return (
         <div className="h-screen w-screen bg-slate-800">
-            <h1 className="m-auto text-3xl font-bold text-center my-5">This is a Blog ID is { id }</h1>
+            <h1 className="m-auto text-3xl font-bold text-center pt-5">This is a Blog ID is { id }</h1>
 
         </div>
     );
