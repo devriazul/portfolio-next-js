344db053e8caef3e8ba647840eb2e2d7651c2088
devriazul <riazul56@gmail.com>
2024-09-07 18:31:45 +0600

    Few updated

--- app/about/page.js
+++ app/about/page.js
@@ -31,7 +31,7 @@ export default function About() {
         {/* Header */}
         <div className="text-center mb-12">
           <h1 className="text-4xl font-bold mb-2">Education and Work Experience</h1>
-          <p className="text-lg text-gray-600">Combination of Skill & Experience</p>
+          <p className="text-lg text-gray-600">Combination of Skill &amp; Experience</p>
         </div>
 
         {/* Education Section */}
