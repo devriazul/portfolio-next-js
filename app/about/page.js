569f5c0bfe7cee1865362be82bcc2c657b986c97
devriazul <riazul56@gmail.com>
2024-09-07 18:29:11 +0600

    Few updates

--- app/about/page.js
+++ app/about/page.js
@@ -1,32 +1,33 @@
 import Image from "next/image";
 import about from "@/public/images/about.png";
-// import Button from "../components/Button";
 
-/* eslint-disable react/no-unescaped-entities */
 export default function About() {
   return (
-    <section class="bg-white dark:bg-gray-900">
-    <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-        <div class="mr-auto place-self-center lg:col-span-7">
-            <h2>About</h2>
-            <h1 class="max-w-2xl my-4 text-2xl font-extrabold tracking-tight leading-none md:text-3xl xl:text-4xl dark:text-white">Riazul Islam</h1>
-            <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Hello, I am Riazul Islam, a web design and development professional. With lots of experience, I have gained extensive expertise in handling client projects and possess strong leadership and communication skills. I am passionate about learning new programming technologies and have a deep love for the field. While I enjoy working with emerging technologies, web development remains my top priority.</p>
-            <a href="#" class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
-                Get started
-                <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-            </a>
-            <a href="#" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
-                Speak to Sales
-            </a> 
+    <section className="bg-white dark:bg-gray-900">
+      <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+        <div className="mr-auto place-self-center lg:col-span-7">
+          <h2>About</h2>
+          <h1 className="max-w-2xl my-4 text-2xl font-extrabold tracking-tight leading-none md:text-3xl xl:text-4xl dark:text-white">Riazul Islam</h1>
+          <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+            Hello, I am Riazul Islam, a web design and development professional. With lots of experience, I have gained extensive expertise in handling client projects and possess strong leadership and communication skills. I am passionate about learning new programming technologies and have a deep love for the field. While I enjoy working with emerging technologies, web development remains my top priority.
+          </p>
+          <a href="#" className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+            Get started
+            <svg className="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+              <path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path>
+            </svg>
+          </a>
+          <a href="#" className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+            Speak to Sales
+          </a>
         </div>
-        <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
-            <Image className="w-full rounded-lg" src={about} alt="mockup" width={500} height={500} />
-            {/* <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png" alt="mockup"> */}
-        </div>                
-    </div>
+        <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
+          <Image className="w-full rounded-lg" src={about} alt="mockup" width={500} height={500} />
+          {/* <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png" alt="mockup"> */}
+        </div>
+      </div>
 
-
-<div className="max-w-7xl mx-auto p-8">
+      <div className="max-w-7xl mx-auto p-8">
         {/* Header */}
         <div className="text-center mb-12">
           <h1 className="text-4xl font-bold mb-2">Education and Work Experience</h1>
@@ -46,7 +47,7 @@ export default function About() {
         {/* Experience Section */}
         <section className="mb-12">
           <h2 className="text-2xl font-bold text-white mb-4">Experience</h2>
-          
+
           {/* Experience 1 */}
           <div className="bg-white shadow-md rounded-lg p-6 mb-4">
             <h3 className="text-xl font-semibold text-gray-900">Sr. Web Developer</h3>
@@ -111,8 +112,6 @@ export default function About() {
           </div>
         </section>
       </div>
-
-</section>
-
+    </section>
   );
 }
